17f998d44d921c076c682ce55c123a7b640d3f5d
nirmalya8 <nirmalya14misra@gmail.com>
2022-10-15 13:25:21 +0530

    Final Logistic Regression Model trained

--- test.py
+++ test.py
@@ -13,15 +13,19 @@ from tfidf import set_paths, read_data, preprocess, training_utils, tfidf
 
 #tfidf = TfidfVectorizer(max_features=300, stop_words="english")
 
-filename = 'finalized_model1.sav'
+filename = 'final_lr1.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-with open('tfidf.pickle','rb') as to_read:
+with open('tfidf2.pickle','rb') as to_read:
     fitted_tfidf = pickle.load(to_read)
 
-w = "Arun Optical"
-w = fitted_tfidf.transform([w])
-print(w)
+while True:
+    w =input()
+    if w == 'b':
+        break
+    w = fitted_tfidf.transform([w])
+    print(w)
 
-pred = loaded_model.predict_proba(w)
-print(pred)
+    pred = loaded_model.predict_proba(w)
+    print(pred)
+    print(loaded_model.predict(w))
--- tfidf.py
+++ tfidf.py
@@ -18,6 +18,9 @@ from sklearn import metrics
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from lightgbm import LGBMClassifier
 import pickle
+import gensim
+from gensim.models import Word2Vec
+
 def set_paths():
     print("[+] Setting paths...")
     base_path = os.getcwd()
@@ -38,7 +41,7 @@ def read_data(file_path):
 def preprocess(data):
     sleep(2)
     print("[+] Preprocessing...")
-    map_dict = {"Food and Groceries": 0, "Medical and Healthcare": 1,"Education":2,"Lifestyle and Entertainment":3,"Travel & Transportation":4,"Clothing":5,"Eye":6,"Shoe":7}#"Housing and Utilities":3
+    map_dict = {"Food and Groceries": 0, "Medical and Healthcare": 1,"Education":2,"Lifestyle and Entertainment":3,"Travel & Transportation":4,"Clothing":5}#,"Eye":6,"Shoe":7}#"Housing and Utilities":3
     data["Category"] = data["Category"].map(map_dict)
     print(map_dict)
     return data
@@ -49,17 +52,30 @@ def training_utils(data):
     xtrain, xtest, ytrain, ytest = train_test_split(
             data['Name'],
             data["Category"],
-            test_size=0.05,
+            test_size=0.30,
             random_state=60,
             stratify=data["Category"],
         )
     return xtrain,xtest,ytrain,ytest
 
+def word_vec(xtrain,xtest):
+    print("[+] Vectorizing...")
+    w2v = Word2Vec(data, min_count = 1, vector_size = 100,window = 5, sg = 1)
+    train_df = pd.DataFrame(
+                tfidf.transform(xtrain).toarray(), columns=tfidf.vocabulary_
+            )
+
+    test_df = pd.DataFrame(
+                tfidf.transform(xtest).toarray(), columns=tfidf.vocabulary_
+            )
+
+    # pickle.dump(tfidf, open("tfidf.pickle", "wb"))
+    return tfidf,train_df, test_df
 #modeling    
 def tfidf(xtrain,xtest):
     sleep(2)
     print("[+] Vectorizing...")
-    tfidf = TfidfVectorizer(max_features=300, stop_words="english")
+    tfidf = TfidfVectorizer(min_df = 5,sublinear_tf=True ,lowercase=True,max_features=300, stop_words="english")
     tfidf = tfidf.fit(xtrain)
     train_df = pd.DataFrame(
                 tfidf.transform(xtrain).toarray(), columns=tfidf.vocabulary_
@@ -69,8 +85,8 @@ def tfidf(xtrain,xtest):
                 tfidf.transform(xtest).toarray(), columns=tfidf.vocabulary_
             )
 
-    pickle.dump(tfidf, open("tfidf.pickle", "wb"))
-    return train_df, test_df
+    # pickle.dump(tfidf, open("tfidf.pickle", "wb"))
+    return tfidf,train_df, test_df
 
 def fit_model(model,train_df,test_df,y_train,ytest):
     sleep(2)
@@ -104,27 +120,77 @@ if __name__=="__main__":
     data = read_data(file_path)
     data = preprocess(data)
     xtrain,xtest, y_train, ytest = training_utils(data)
-    train_df,test_df = tfidf(xtrain,xtest)
+    fitted_tfidf, train_df,test_df = tfidf(xtrain,xtest)
     st_x= StandardScaler()    
     train_df = st_x.fit_transform(train_df)    
     test_df = st_x.transform(test_df)    
     
     # model = KNeighborsClassifier(n_neighbors=63)
     # knn = fit_model(model,train_df,test_df,y_train,ytest)
-    # model = DecisionTreeClassifier()
-    # dt = fit_model(model,train_df,test_df,y_train,ytest)
-    # model = RandomForestClassifier(n_estimators= 300, criterion="entropy")
+   
+    #model = DecisionTreeClassifier()
+    #dt = fit_model(model,train_df,test_df,y_train,ytest)
+   
+    #model = RandomForestClassifier(n_estimators= 300, criterion="entropy")
     # rf = fit_model(model,train_df,test_df,y_train,ytest)
 
     # final_model = VotingClassifier(estimators=[('dt',dt),('rf',rf)])
     # vote = fit_model(final_model,train_df,test_df,y_train,ytest)
-    model = XGBClassifier(booster = 'dart')
-    xgb = fit_model(model,train_df,test_df,y_train,ytest)
     
-    model  = XGBClassifier()
-    xgb = fit_model(model,train_df,test_df,y_train,ytest)
-    filename = 'finalized_model1.sav'
-    pickle.dump(xgb, open(filename, 'wb'))
+    
+#    model = XGBClassifier(booster = 'dart')
+#    xgb = fit_model(model,train_df,test_df,y_train,ytest)
+    
+    # model  = XGBClassifier()
+    # xgb = fit_model(model,train_df,test_df,y_train,ytest)
+    a=0
+#    for row in test_df:
+#        print(xgb.predict_proba(row.reshape(1,-1)))
+#        print(xgb.predict(row.reshape(1,-1)))
+#        a+=1
+#        if a ==10:
+#            break
+    
+#    pickle.dump(fitted_tfidf,open("tfidf2.pickle",'wb'))
+    w = ["Arun Clothes","Pooja Medical","ABC Tours and Travels", "Dev Super Mart"]
+    w1 = []
+    for i in w:
+         w1.append(fitted_tfidf.transform([i]))
+    print(w1)
+    
+    w2 = []
+    for i in w:
+        w2.append(fitted_tfidf.transform([i]))
+    print(w2)
+
+
+    from sklearn.linear_model import LogisticRegression
+    #multi_class="multinomial"
+    model = LogisticRegression(solver='saga', max_iter=300,multi_class="multinomial",warm_start=True)
+    lr = fit_model(model,train_df,test_df,y_train,ytest)
+    print("LR")
+    for i in range(len(w1)):
+         print(w[i])
+         print(lr.predict(w1[i]))
+         print(lr.predict_proba(w1[i]))
+
+    #model = KNeighborsClassifier(n_neighbors=75)
+    #knn = fit_model(model,train_df,test_df,y_train,ytest)
+    #print("KNN")
+    #for i in range(len(w1)):
+    #     print(w[i])
+    #     print(knn.predict(w1[i]))
+    #     print(knn.predict_proba(w1[i]))
+
+    #print("DT")
+    #for i in range(len(w1)):
+    #     print(w[i])
+    #     print(dt.predict(w1[i]))
+    #     print(dt.predict_proba(w1[i]))
+    filename = 'final_lr1.sav'
+
+
+ #   pickle.dump(lr, open(filename, 'wb'))
     # model = XGBClassifier(colsample_bytree=0.6059329304964837,
     #                         gamma=2.361923398781385,
     #                         max_depth=12,
@@ -137,8 +203,12 @@ if __name__=="__main__":
     # xgb = fit_model(model,train_df,test_df,y_train,ytest)
 
    
-    # final_model = VotingClassifier(estimators=[('rf',rf),('xgb',xgb)])
+    # final_model = VotingClassifier(estimators=[('dt',dt),('knn',knn),('lr',lr)])
     # final_model = fit_model(final_model,train_df,test_df,y_train,ytest)
+    # print("Final")
+    # for i in range(len(w1)):
+    #     print(w[i])
+    #     print(dt.predict_proba(w1[i]))
 
     # trials = Trials()
     # space={'max_depth': hp.quniform("max_depth", 3, 18, 1),
