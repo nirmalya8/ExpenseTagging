6b1d2c81fd5fa871ccf064929b52d53fb9bd832e
nirmalya8 <nirmalya14misra@gmail.com>
2022-10-21 23:06:43 +0530

    Added Model

--- test.py
+++ test.py
@@ -34,36 +34,42 @@ for k in data.keys():
 #if bo:
 #    print(categories[ind])
 #print(categories[b])
+filename = 'final_lr1.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
+with open('tfidf2.pickle','rb') as to_read:
+   fitted_tfidf = pickle.load(to_read)
+
+map_dict = {0:"Food and Groceries", 1:"Medical and Healthcare",2:"Education",3:"Lifestyle and Entertainment",4:"Travel & Transportation",5:"Clothing"}
+
+import time
 with st.form("form1",clear_on_submit=False):
     brand = st.text_input("Enter the name of the brand")
     submit = st.form_submit_button('Submit')
     if submit:
         st.subheader("Output Text")
         with st.spinner(text="This may take a moment..."):
+            time.sleep(2)
             bo,ind = is_str_in(brand,brands)
             if bo:
                 out = categories[ind]
-
+ 
             else:
-                a,out,_,_=find_closest_match(brand,brands)
-                out = categories[out]
+                a,out1,_,_=find_closest_match(brand,brands)
+                w = fitted_tfidf.transform([brand])
+                    # print(w)
+
+                pred = loaded_model.predict(w)
+                out = map_dict[pred[0]]
+                out = "Normal String matching:"+str(categories[out1])+"\n"+" Model:"+out
+                # print(loaded_model.predict(w))
+                #out = categories[out]
         st.write(out)
     
 #'''
-#filename = 'final_lr1.sav'
-#loaded_model = pickle.load(open(filename, 'rb'))
-
-#with open('tfidf2.pickle','rb') as to_read:
- #   fitted_tfidf = pickle.load(to_read)
 
 #while True:
     #w =input()
     #if w == 'b':
     #    break
-    #w = fitted_tfidf.transform([w])
-    #print(w)
-
-    #pred = loaded_model.predict_proba(w)
-    #print(pred)
-    #print(loaded_model.predict(w))'''
+#'''
