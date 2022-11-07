27709ae91682ed35d563831b18b14b3874decd39
nirmalya8 <nirmalya14misra@gmail.com>
2022-11-08 03:51:06 +0530

    Added support for files

--- test.py
+++ test.py
@@ -42,29 +42,59 @@ with open('tfidf2.pickle','rb') as to_read:
 
 map_dict = {0:"Food and Groceries", 1:"Medical and Healthcare",2:"Education",3:"Lifestyle and Entertainment",4:"Travel & Transportation",5:"Clothing"}
 
-import time
-with st.form("form1",clear_on_submit=False):
-    brand = st.text_input("Enter the name of the brand")
-    submit = st.form_submit_button('Submit')
-    if submit:
-        st.subheader("Output Text")
-        with st.spinner(text="This may take a moment..."):
-            time.sleep(2)
-            bo,ind = is_str_in(brand,brands)
-            if bo:
-                out = categories[ind]
+def predict_model(brand):
+    bo,ind = is_str_in(brand,brands)
+    if bo:
+        out = categories[ind]
  
-            else:
-                a,out1,_,_=find_closest_match(brand,brands)
-                w = fitted_tfidf.transform([brand])
+    else:
+        w = fitted_tfidf.transform([brand])
                     # print(w)
 
-                pred = loaded_model.predict(w)
-                out = map_dict[pred[0]]
-                out = "Normal String matching:"+str(categories[out1])+"\n"+" Model:"+out
+        pred = loaded_model.predict(w)
+        out = map_dict[pred[0]]
+    return out
                 # print(loaded_model.predict(w))
                 #out = categories[out]
-        st.write(out)
+
+import time
+# brand = st.text_input("Enter the name of the brand")
+#     submit = st.form_submit_button('Submit')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    uploaded_file = uploaded_file.getvalue().decode('utf-8').splitlines()
+        # st.write(uploaded_file)
+
+# print the list
+    #print(content_list)
+
+# remove new line characters
+    brand_list = [x.strip() for x in uploaded_file]
+        #st.write(" ".join(content_list))
+    st.subheader("Output Text")
+    with st.spinner(text="This may take a moment..."):
+                time.sleep(2)
+                out_list = []
+                for brand in brand_list:
+                    out_list.append(brand+" -> "+predict_model(brand))
+                
+                # bo,ind = is_str_in(brand,brands)
+                # if bo:
+                #     out = categories[ind]
+    
+                # else:
+                #     a,out1,_,_=find_closest_match(brand,brands)
+                #     w = fitted_tfidf.transform([brand])
+                #         # print(w)
+
+                #     pred = loaded_model.predict(w)
+                #     out = map_dict[pred[0]]
+                #     out = "Normal String matching:"+str(categories[out1])+"\n"+" Model:"+out
+                    # print(loaded_model.predict(w))
+                    #out = categories[out]
+
+    out = "\n".join(out_list)
+    st.download_button('Download Outputs', out) 
     
 #'''
 
